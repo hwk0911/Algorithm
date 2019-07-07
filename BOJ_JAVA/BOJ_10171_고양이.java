5e4d19f3897dc4d190e3c0d0f985284585a32c96
master <hwk0911@gmail.com>
2019-07-07 16:31:20 +0900

    ADD : BOJ10171

---
+++ BOJ_JAVA/BOJ_10171_고양이.java
@@ -0,0 +1,14 @@
+public class BOJ_10171_고양이 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.print(" \\(__)|");
+    }
+}
+/*
+\    /\
+ )  ( ')
+(  /  )
+ \(__)|
+* */
