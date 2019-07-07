8ac4e7b7d4e5be68d65d77d743929423c47e9b0a
master <hwk0911@gmail.com>
2019-07-07 13:36:53 +0900

    ADD : BOJ15963

---
+++ BOJ_JAVA/BOJ_15963_CASIO.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class BOJ_15963_CASIO {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String N , M;
+
+        N = sc.next();
+        M = sc.next();
+
+        if(N.equals(M))
+            System.out.println(1);
+        else
+            System.out.println(0);
+    }
+}
