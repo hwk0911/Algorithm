c95c3604b31784a861ca5e8d8506114dda09e620
master <hwk0911@gmail.com>
2019-07-07 13:16:02 +0900

    ADD : BOJ15964

---
+++ BOJ_JAVA/BOJ_15964_이상한기호.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class BOJ_15964_이상한기호 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long A, B;
+
+        A = sc.nextInt();
+        B = sc.nextInt();
+
+        System.out.println((A*A)-(B*B));
+    }
+}
