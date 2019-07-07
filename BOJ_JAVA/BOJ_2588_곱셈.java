a728403ec69e91ed139f1f852eef0e4a63ea5b3c
master <hwk0911@gmail.com>
2019-07-07 16:41:28 +0900

    ADD : BOJ2588

---
+++ BOJ_JAVA/BOJ_2588_곱셈.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class BOJ_2588_곱셈 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Integer A, B;
+
+        A = sc.nextInt();
+        B = sc.nextInt();
+
+        System.out.println(A * (B % 10));
+        System.out.println(A * (B / 10 % 10));
+        System.out.println(A * (B / 100));
+
+        System.out.println(A * B);
+    }
+}
