39d66a9ebda2bffcb07993edbc9d28150d228a0b
excelK <hwk0911@gmail.com>
2019-08-04 14:06:38 +0900

    ADD BOj 3053

---
+++ BOJ_JAVA/BOJ_3053_택시기하학.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_3053_택시기하학 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Integer R = Integer.parseInt(br.readLine());
+
+        System.out.printf("%.6f\n", R*R*Math.PI);
+        System.out.printf("%.6f\n",(double)R*R*2);
+    }
+}
